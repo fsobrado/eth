92d4a1cfbbe69e6f542be8bed5ec5fb5aea8592b
fsobrado <fede_sobrado@yahoo.com>
2024-09-21 12:10:31 -0600

    Update NFTMarketplace.sol

--- NFTMarketplace.sol
+++ NFTMarketplace.sol
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+
+pragma solidity 0.8.22;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract NFTMarketplace is ERC721 {
-    // Incremental ID for minting NFTs
+    // Cada vez que se crea un nft, incrementatamos el _nextTokenId
     uint256 private _nextTokenId = 1;
     
-    // Struct to represent each item for sale
+    // Este struct va a representar el item(nft)
     struct NFTItem {
         uint256 tokenId;
         address owner;
@@ -15,27 +16,26 @@ contract NFTMarketplace is ERC721 {
         bool isForSale;
     }
     
-    // Mapping of tokenId to NFTItem
+    // Listamos los nft que ya han sido creados y esta disponibles para la venta
     mapping(uint256 => NFTItem) public nftItems;
     
-    // Event emitted when a new item is created
+    // Eventos para cada accion
     event NFTCreated(uint256 indexed tokenId, address owner, uint256 price);
-    
-    // Event emitted when an item is bought
     event NFTBought(uint256 indexed tokenId, address buyer, uint256 price);
     
     constructor() ERC721("MyNFT", "MNFT") {}
 
-    // Function to create a new NFT
+    // Funcion que crea los nuevos nft, los mintea
     function createNFT(uint256 price) public returns (uint256) {
-        require(price > 0, "Price must be greater than zero");
+        require(price > 0, "Precio debe de ser mayor a cero");
 
+        ///Aca solo referenciamos la variable tipo contador _nextTokenId, para dar un id al nuevo nft
         uint256 newItemId = _nextTokenId;
 
-        // Mint the NFT
+        // Minteo
         _mint(msg.sender, newItemId);
 
-        // Add the NFT to the marketplace
+        // Agregamos el nuevo nft al lista del marketplace
         nftItems[newItemId] = NFTItem({
             tokenId: newItemId,
             owner: msg.sender,
@@ -45,47 +45,49 @@ contract NFTMarketplace is ERC721 {
 
         emit NFTCreated(newItemId, msg.sender, price);
 
-        // Increment the token ID for the next NFT
+        // Seteamos la variable que lleva el control de los ids a +1
         _nextTokenId++;
 
         return newItemId;
     }
 
-    // Function to buy an NFT
+    // Comprar un nft del marketplace
     function buyNFT(uint256 tokenId) public payable {
         NFTItem storage item = nftItems[tokenId];
-        require(item.isForSale, "This NFT is not for sale");
-        require(msg.value >= item.price, "Insufficient funds to buy this NFT");
+        require(item.isForSale, "Este NFT no esta a la venta");
+        require(msg.value >= item.price, "Fondos insuficientes para la compra");
 
+        //Aca lo que hacemos es pasar el ownership del creador del nft al comprador
         address previousOwner = item.owner;
         address newOwner = msg.sender;
 
-        // Transfer ownership of the NFT
+        // Llamado a ejecutar el paso de ownership
         _transfer(previousOwner, newOwner, tokenId);
 
-        // Update the NFT item
+        // Le decimos al item (nft) que su dueño es el comprador y la bandera que indicaba que estaba a la venta de pone en false
         item.owner = newOwner;
         item.isForSale = false;
 
-        // Transfer the funds to the previous owner
+        // Finalmenete pagamos el creador del nft
         payable(previousOwner).transfer(msg.value);
 
         emit NFTBought(tokenId, newOwner, item.price);
     }
 
-    // Function to list an NFT for sale
-    function listNFTForSale(uint256 tokenId, uint256 price) public {
-        require(ownerOf(tokenId) == msg.sender, "Only the owner can list this NFT");
-        require(price > 0, "Price must be greater than zero");
+    // Agregar o rehabilitar un nft en particular, por ejemplo si yo compre un nft y luego lo quiero revender llamo este metodo,
+    // ya que el nft con su id original siempre va a estar aqui de forma inmutable
+    function addNFTForSale(uint256 tokenId, uint256 price) public {
+        require(ownerOf(tokenId) == msg.sender, "Solamenete el dueno puede poner a la venta este nft");
+        require(price > 0, "Precio debe de ser mayor a cero");
 
         NFTItem storage item = nftItems[tokenId];
         item.isForSale = true;
         item.price = price;
     }
 
-    // Function to cancel the sale of an NFT
+    // Cancelamos la venta del nft
     function cancelNFTSale(uint256 tokenId) public {
-        require(ownerOf(tokenId) == msg.sender, "Only the owner can cancel this sale");
+        require(ownerOf(tokenId) == msg.sender, "Solamenete el dueno puede cancelar la venta de este nft");
 
         NFTItem storage item = nftItems[tokenId];
         item.isForSale = false;
